58b64a6fb9c996d9ef0e05842f2c0334bdf6558f
Subrat Mainali <sm63966@cs.utexas.edu>
2017-09-22 23:05:14 -0500

    added scroll...

--- gtestm/gui/gui.py
+++ gtestm/gui/gui.py
@@ -11,15 +11,38 @@ def do_full_refresh():
     print ("Full Refresh Requested")
 
 def get_tests():
-    return {"testasdasd": "PASS", "testasfasfasfsaf": "PASS", "testasfasfasdasdasdasdasdasfsafasf": "FAIL"}
+    hash_ = {}
+    for element in range(100):
+        status = "PASS"
+        if element % 2 == 0:
+            if element % 4 == 0:
+                status = "COMPILE ERR"
+            else:
+                status = "FAIL"
+        if element % 7 == 0:
+            status = "INVALID"
+
+        hash_["test_Test_test" + str(element)] = status
+    return hash_
 
 
 class Overview(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.master = master
+    def __init__(self, root):
 
-        master.title("Overview")
+        tk.Frame.__init__(self, root)
+        self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw", 
+                                  tags="self.frame")
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.master = self.frame
 
         self.tests_widgets = []
 
@@ -28,7 +51,7 @@ class Overview(tk.Frame):
             log_in.mainloop()
             log_in.master.withdraw()
 
-        self.refresh = ttk.Button(master, text="Refresh", command=lambda: self.update("refresh"))
+        self.refresh = ttk.Button(self.master, text="Refresh", command=lambda: self.update("refresh"))
 
         self.render_layout()
 
@@ -40,6 +63,9 @@ class Overview(tk.Frame):
 
     def render_tests(self):
 
+        self.foreground = {"PASS" : "white", "FAIL" : "white", "INVALID" : "white", "COMPILE ERR" : "white"}
+        self.background = {"PASS" : "green", "FAIL" : "red", "INVALID" : "blue", "COMPILE ERR" : "blue"}
+
         # get rid of all the current tests
         for test in self.tests_widgets:
             test.grid_forget()
@@ -47,48 +73,60 @@ class Overview(tk.Frame):
         tests_ = get_tests()
         passing_tests = filter(lambda x: tests_[x] == "PASS", tests_.keys())
         failing_tests = filter(lambda x: tests_[x] == "FAIL", tests_.keys())
+        invalid_tests = filter(lambda x: tests_[x] == "INVALID", tests_.keys())
+        comperr_tests = filter(lambda x: tests_[x] == "COMPILE ERR", tests_.keys())
 
-        index = 0
-        for key in passing_tests:
-            index += 1
-            test_hash = ttk.Label(self.master, text=key)
-            test_status = ttk.Label(self.master, text=tests_[key])
+        self.render_test_index = 0
 
-            test_status.winfo_rgb("green")
+        # tests are displayed in this order on the screen
+        for test_hash in passing_tests:
+            self.render_test(test_hash, tests_[test_hash])
 
-            test_hash.grid(row=index, column=0, columnspan=3, sticky=tk.W)
-            test_status.grid(row=index, column=4, sticky=tk.E)
+        for test_hash in failing_tests:
+            self.render_test(test_hash, tests_[test_hash])
 
-            self.tests_widgets.append(test_hash)
-            self.tests_widgets.append(test_status)
+        for test_hash in invalid_tests:
+            self.render_test(test_hash, tests_[test_hash])
 
-        for key in failing_tests:
-            index += 1
-            test_hash = ttk.Label(self.master, text=key)
-            test_status = ttk.Label(self.master, text=tests_[key])
+        for test_hash in comperr_tests:
+            self.render_test(test_hash, tests_[test_hash])
 
-            test_hash.grid(row=index, column=0, columnspan=3, sticky=tk.W)
-            test_status.grid(row=index, column=4, sticky=tk.E)
+    def render_test(self, test_hash, test_status):
+        print ("called")
+        self.render_test_index += 1
 
-            self.tests_widgets.append(test_hash)
-            self.tests_widgets.append(test_status)
+        test_hash_label = ttk.Label(self.master, text=test_hash)
+        test_status_label = ttk.Label(self.master, text=test_status)
+
+        test_status_label.configure(foreground=self.foreground[test_status])
+        test_status_label.configure(background=self.background[test_status])
+
+        test_hash_label.grid(row=self.render_test_index, column=0, columnspan=3, sticky=tk.W)
+        test_status_label.grid(row=self.render_test_index, column=4, sticky=tk.E)
+
+        self.tests_widgets.append(test_hash_label)
+        self.tests_widgets.append(test_status_label)
+        return 0
 
     def update(self, method):
 
         if method == "refresh":
             do_full_refresh()
 
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
 class LoginFrame(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
 
-        # CONTENT
         self.master = master
 
-        master.title("G Test Manager")
+        aster.title("G Test Manager")
 
-        self.ut_id = ttk.Label(master, text="UT ID:")
+        self.ut_id = ttk.Label(master, text="CS ID:")
         self.password = ttk.Label(master, text="Password:")
 
         vcmd_1 = master.register(self.validate_ut_id) # we have to wrap the command
@@ -132,6 +170,7 @@ class LoginFrame(tk.Frame):
         elif method == "quit":
             self.master.master.destroy()
 
-root = tk.Tk()
-overview = Overview(root)
-overview.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    Overview(root)
+    root.mainloop()
